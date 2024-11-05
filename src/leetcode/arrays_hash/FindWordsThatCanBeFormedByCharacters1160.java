d06e5aac8f6b1e58183d23a74fc69554789fab76
sardoriiiy <leo@indev.expert>
2024-11-05 16:58:40 +0500

    countCharacters

---
+++ src/leetcode/arrays_hash/FindWordsThatCanBeFormedByCharacters1160.java
@@ -0,0 +1,30 @@
+package leetcode.arrays_hash;
+
+import java.util.Arrays;
+
+public class FindWordsThatCanBeFormedByCharacters1160 {
+
+    public int countCharacters(String[] words, String chars) {
+        int[] ltrs = new int[26];
+        for (char ch : chars.toCharArray()) {
+            ltrs[ch - 'a']++;
+        }
+        int sumOfLengths = 0;
+        for (String word : words) {
+            boolean isGood = true;
+            int[] ltrs1 = new int[26];
+            System.arraycopy(ltrs, 0, ltrs1, 0, 26);
+            for (char ch : word.toCharArray()) {
+                if (ltrs1[ch - 'a'] == 0) {
+                    isGood = false;
+                } else {
+                    ltrs1[ch - 'a']--;
+                }
+            }
+            if (isGood) {
+                sumOfLengths += word.length();
+            }
+        }
+        return sumOfLengths;
+    }
+}
